a61c91f925f0d1e225df5d9f5adf2a9d8c84e6aa
cidarosa <lucita.rosa@gmail.com>
2023-11-28 17:23:28 -0300

    Desafio empregados autenticação

---
+++ src/main/java/com/devsuperior/demo/entities/User.java
@@ -0,0 +1,105 @@
+package com.devsuperior.demo.entities;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    //associação - somente o usuario conhece os roles - unidirecional
+    // associação Muitos para muitos
+    @ManyToMany(fetch = FetchType.EAGER) //forçar carregar os roles
+    @JoinTable(name = "tb_user_role",
+    joinColumns = @JoinColumn(name = "user_id"), //PK ref tabela onde estou
+    inverseJoinColumns = @JoinColumn(name = "role_id")) //chave do outro lado
+    private Set<Role> roles = new HashSet<>();
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    // ****************
+    //métodos adicionais
+    public void addRole(Role role) {
+        roles.add(role);
+    }
+
+    public boolean hasRole(String roleName) {
+        for (Role role : roles) {
+            if (role.getAuthority().equals(roleName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    // *****************
+}
